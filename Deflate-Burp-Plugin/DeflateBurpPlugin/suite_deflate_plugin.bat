dc0d821c767978f9d40088e5cb12c18b22d349ef
Meatballs <eat_meatballs@hotmail.co.uk>
2013-05-17 18:19:42 +0100

    Remove folder

--- Deflate-Burp-Plugin/DeflateBurpPlugin/suite_deflate_plugin.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem The debug command-line option is used by DeflateBurpPlugin.jar. 
-rem Remove it to disable plug-in debugging information.
-java -Xmx256m -classpath burpsuite_v1.1.jar;DeflateBurpPlugin.jar burp.StartBurp debug
